044bd3891a486a9706276ddc4128908c910fa68c
Skyler Burger <skylerburger@gmail.com>
2018-12-14 11:09:19 -0800

    weather DB working

--- schema.sql
+++ schema.sql
@@ -5,8 +5,8 @@ CREATE TABLE locations (
   id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
-  latitude NUMERIC(8, 6),
-  longitude NUMERIC(8, 6)
+  latitude NUMERIC,
+  longitude NUMERIC
 );
 
 CREATE TABLE weather (
